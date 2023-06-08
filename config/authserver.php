9830a03502e9227e4feb9c30179d3ebf61972e51
Wouter de Berg <wouterdeberg@gmail.com>
2023-06-08 11:06:55 +0200

    Added Auth Server client_id

--- config/authserver.php
+++ config/authserver.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'client_id' => env('AUTH_SERVER_CLIENT_ID', null),
     'token' => env('AUTH_SERVER_TOKEN', null),
     'public_key' => env('AUTH_SERVER_PUBLIC_KEY_FILE', 'auth-server-public.key'),
 ];
