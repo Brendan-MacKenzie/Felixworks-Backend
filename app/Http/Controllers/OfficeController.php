65c8bfc02fe7f7cf6c7d4b13c4046477a61e4751
Ivan Levakov <ivan.n.levakov@gmail.com>
2023-06-08 16:25:11 +0200

    Offices Crud

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Office;
 use Illuminate\Http\Request;
+use App\Services\OfficeService;
+use Illuminate\Support\Facades\Validator;
 
 class OfficeController extends Controller
 {
+    private $officeService;
+
+    public function __construct(OfficeService $officeService)
+    {
+        $this->officeService = $officeService;
+    }
+
     //store office
     public function store(Request $request)
     {
-        // Validate the incoming request
-        $validatedData = $request->validate([
+        // Validate input
+        $validator = Validator::make($request->all(), [
             'agency_id' => 'required|integer|exists:agencies,id',
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
@@ -22,13 +32,108 @@ class OfficeController extends Controller
             'zip_code' => 'required|string|max:20',
             'city' => 'required|string|max:100',
             'country' => 'required|string|max:100',
-            'created_by' => 'required|integer|exists:users,id',
         ]);
 
-        // Create the office using the validated data
-        $office = Office::create($validatedData);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => __('exceptions.validation'),
+                'issue' => $validator->failed(),
+                'errors' => $validator->errors(),
+            ], 400);
+        }
 
-        // Return the created office as JSON response:
-        return response()->json($office, 201);
+        try {
+            $office = $this->officeService->store($request->only([
+                'agency_id',
+                'name',
+                'description',
+                'website',
+                'phone',
+                'street_name',
+                'number',
+                'zip_code',
+                'city',
+                'country',
+            ]));
+        } catch (Exception $exception) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $office,
+        ], 201);
+    }
+
+    public function update(Request $request, Office $office)
+    {
+        // Validate input
+        $validator = Validator::make($request->all(), [
+            'agency_id' => 'required|integer|exists:agencies,id',
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'website' => 'nullable|url',
+            'phone' => 'nullable|string|max:20',
+            'street_name' => 'required|string|max:255',
+            'number' => 'required|string|max:20',
+            'zip_code' => 'required|string|max:20',
+            'city' => 'required|string|max:100',
+            'country' => 'required|string|max:100',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => __('exceptions.validation'),
+                'issue' => $validator->failed(),
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        try {
+            $office = $this->officeService->update($request->only([
+                'agency_id',
+                'name',
+                'description',
+                'website',
+                'phone',
+                'street_name',
+                'number',
+                'zip_code',
+                'city',
+                'country',
+            ]), $office->id);
+        } catch (Exception $exception) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $office,
+        ], 200);
+    }
+
+    public function destroy(Office $office)
+    {
+        try {
+            $this->officeService->delete($office->id);
+        } catch (Exception $exception) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Óffice removed successfully',
+        ], 200);
     }
 }
