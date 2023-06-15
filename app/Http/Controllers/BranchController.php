c20562540ab6aa74cb10a4367e79da7e871e719e
Ivan Levakov <ivan.n.levakov@gmail.com>
2023-06-15 13:34:50 +0200

    [FW-151]

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -17,6 +17,38 @@ class BranchController extends Controller
         $this->branchService = $branchService;
     }
 
+    public function index(Request $request)
+    {
+        $validator = Validator::make(request()->all(), [
+            'search' => 'string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => __('exceptions.validation'),
+                'issue' => $validator->failed(),
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $search = $request->input('search', null);
+
+        try {
+            $branches = $this->branchService->list(0, $search);
+        } catch (Exception $exception) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $branches,
+        ], 200);
+    }
+
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
